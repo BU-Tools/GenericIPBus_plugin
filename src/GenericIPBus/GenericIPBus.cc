e8ffde3bd0ae1bb47d9ee10172f073a5976cfa33
Amitav Mitra <amitav.mitra@cern.ch>
2021-06-10 12:02:34 -0400

    set up RegisterHelper's TextIO pointer

--- src/GenericIPBus/GenericIPBus.cc
+++ src/GenericIPBus/GenericIPBus.cc
@@ -1,7 +1,9 @@
 #include <GenericIPBus/GenericIPBus.hh>
 #include <fstream> //std::ofstream
 
-GenericIPBus::GenericIPBus():IPBusConnection("GenericIPBus"){  
+GenericIPBus::GenericIPBus():IPBusConnection("GenericIPBus"){
+    // give BUTool::RegisterHelper it's TextIO pointer
+    SetupTextIO();
 }
 
 GenericIPBus::~GenericIPBus(){
